3c5dc9900a2a2dd3b27ee869c3bbf39cfbbd361a
Alan Orth <a.orth@cgiar.org>
2010-06-27 15:05:00 +0300

    Add basic FLV support using the flowplayer SWF player.

--- index.php
+++ index.php
@@ -16,7 +16,10 @@
 <div id='aboutus'>
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. <a href="#">Testing links</a>.
 </div>
-<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><a href="videos/andrew.flv" style="display:block;width:260px;height:300px" id="player"><img src="malcolmx.jpg" /></a></div>
+<script>
+	flowplayer("player","flowplayer-3.2.2.swf");
+</script>
 <div class='person'><img src="malcolmx.jpg" /></div>
 <div class='person'><img src="malcolmx.jpg" /></div>
 <div class='person'><img src="malcolmx.jpg" /></div>
