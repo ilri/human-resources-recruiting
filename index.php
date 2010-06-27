9658b09f9588a8a81f56e0961346db0af832cf68
Alan Orth <a.orth@cgiar.org>
2010-06-27 14:10:35 +0300

    Updated page width to 960px to reflect the ILRI banner Updated with more current styles from "ILRI" look and feel

--- index.php
+++ index.php
@@ -9,18 +9,20 @@
 <div id='outer'>
 <div id='header'>
 <?php require_once('navigation.php'); ?>
+<div id="banner"></div>
 </div> <!-- //header -->
 
 <div id='inner'>
 <div id='aboutus'>
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. <a href="#">Testing links</a>.
 </div>
-<div class='person'><img src="malcolmx.jpg" style="border: none;" /></div>
-<div class='person'><img src="malcolmx.jpg" style="border: none;" /></div>
-<div class='person left'><img src="malcolmx.jpg" style="border: none;" /></div>
-<div class='person'><img src="malcolmx.jpg" style="border: none;" /></div>
-<div class='person'><img src="malcolmx.jpg" style="border: none;" /></div>
-<div class='person'><img src="malcolmx.jpg" style="border: none;" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
+<div class='person'><img src="malcolmx.jpg" /></div>
 </div> <!-- //inner -->
 
 <div id='footer'>
--- navigation.php
+++ navigation.php
@@ -6,8 +6,8 @@ $page = basename($_SERVER['PHP_SELF']);
 
 <div id="navcontainer">
 <ul id="navlist">
-<li><a href="index.php"<?php if($page == 'index.php') echo 'id="current"'; ?>>ilri</a></li>
-<li><a href="todo.php"<?php if($page == 'todo.php') echo 'id="current"'; ?>>to do</a></li>
-<li><a href="people.php" <?php if($page == 'people.php') echo 'id="current"'; ?>>people</a></li>
+<li><a href="http://www.ilri.org/" title="Home"?>Home</a></li>
+<li><a href="todo.php"<?php if($page == 'todo.php') echo 'id="current"'; ?>>To do</a></li>
+<li><a href="people.php" <?php if($page == 'people.php') echo 'id="current"'; ?>>People</a></li>
 </ul>
 </div>
