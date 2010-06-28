025f658d3010c992b604489140d0e08f37ea9754
Alan Orth <a.orth@cgiar.org>
2010-06-28 16:45:22 +0300

    The videos won't play if there is an invalid image in the div... oops.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <div id='aboutus'>
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. <a href="#">Testing links</a>.
 </div>
-<div id="player" class='person'><img src="malcolmx.jpg" /></div>
+<div id="player" class='person'><img src="images/mugshots/steve_k.png" /></div>
 
 <div class='person'><img src="images/mugshots/steve_k.png" /></div>
 <div class='person'><img src="images/mugshots/steve_k.png" /></div>
