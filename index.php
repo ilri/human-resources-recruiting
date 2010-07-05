11e8cb96a2c57508c894f97b38f588af916dbaf6
Alan Orth <a.orth@cgiar.org>
2010-07-05 11:17:34 +0300

    Add check for POSTed 'sortby' parameter, default to random if not specified

--- index.php
+++ index.php
@@ -16,6 +16,19 @@
 <div id='aboutus'>
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. <a href="#">Testing links</a>.
 </div>
+
+<?php
+// determine how to sort our people!
+// first see if a "sortby" value exists, otherwise just sort randomly
+$item = 'sortby';
+if (!empty($_POST[$item])) {
+		$sortby = $_POST[$item];
+}
+else {
+		$sortby = 'random';
+}
+?>
+
 <div id="player" class='person'><img src="images/mugshots/2andrew better.png" /></div>
 
 <div class='person'><img src="images/mugshots/steve_k.png" /></div>
