67f6cc25741ae0c72f1c22e6071dde2cc2e0b1ec
Alan Orth <a.orth@cgiar.org>
2010-07-06 08:46:08 +0300

    Add jquery 1.4.2 so we can work more dynamically with flashembed

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 <head>
 	<title>ILRI - International Livestock Research Institutute</title>
 	<?php require_once('head_includes.php'); ?>
+	<script>
+		$(function() {
+			$('#player').flashembed("videos/player_flv_maxi.swf", {
+				flv: 'andrew.flv', //relative to player!
+				showplayer: 'never',
+				showloading: 'never',
+				margin: '0',
+				startimage: 'images/mugshots/andrew_play.png'});
+		});
+	</script>
 </head>
 <body>
 
@@ -24,31 +34,6 @@ It is a long established fact that a reader will be distracted by the readable c
 <div class='person'><img src="images/mugshots/steve_k.png" /></div>
 <div class='person' id="player2"><img src="images/mugshots/2andrew better.png" /></div>
 <div class='person'><img src="images/mugshots/steve_k.png" /></div>
-<script type="text/javascript">
-	flashembed("player","videos/player_flv_maxi.swf", {
-		flv: 'andrew.flv', //relative to player!
-		showplayer: 'never',
-		showloading: 'never',
-		margin: '0',
-		startimage: 'images/mugshots/andrew_play.png'
-		}
-	);
-
-//	flashembed("player2","videos/flayr.swf", {
-//		movie: 'andrew.flv', //relative to player!
-//		autoplay: 'true',
-//		controls: 'hide'
-//		}
-//	);
-//
-//	flashembed("player3","videos/flayr.swf", {
-//		movie: 'andrew.flv', //relative to player!
-//		autoplay: 'true',
-//		controls: 'hide'
-//		}
-//	);
-
-</script>
 </div> <!-- //inner -->
 
 <div id='footer'>
