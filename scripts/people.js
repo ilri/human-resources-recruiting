9a8d875a17493adafed428a6ffe755eb859a8e4b
Alan Orth <a.orth@cgiar.org>
2010-08-31 10:46:43 +0300

    Add initial swapping support for featured people New code in scripts/people.js and also some new stuff in index.php (we convert our "featured" people array from PHP to Javascript)

--- scripts/people.js
+++ scripts/people.js
@@ -29,4 +29,74 @@ $(document).ready(function() {
 				}
 			});
 	});
+
+	// swap featured people
+	$('#features img.person1').click(function () {
+		$f().unload();
+
+		// store the old values
+		var name = featured[0]['name'];
+		var description = featured[0]['description'];
+		var startimage = featured[0]['startimage'];
+		var video = featured[0]['video'];
+		var image = featured[0]['image'];
+
+		// replace the values in the DOM
+		$('#crowdMemberName').html(featured[1]['name'].toLowerCase());
+		$('#crowdMemberDescription').html(featured[1]['description']);
+		$('#mainFeature img.person').attr('src',featured[1]['image']).attr('title',featured[1]['name']).attr('alt',featured[1]['name']);
+		$('#featured').attr('href',featured[1]['video']);
+		$('#featured').css('background-image','url('+featured[1]['startimage']+')');
+
+		// swap the featured person's image out, into the "small"
+		$('#features img.person1').attr('src',image).attr('title',name).attr('alt',name);
+
+		// swap the values in the array (1 -> 0, 0 -> 1)
+		featured[0]['name'] = featured[1]['name'];
+		featured[0]['description'] = featured[1]['description'];
+		featured[0]['startimage'] = featured[1]['startimage'];
+		featured[0]['video'] = featured[1]['video'];
+		featured[0]['image'] = featured[1]['image'];
+
+		featured[1]['name'] = name;
+		featured[1]['description'] = description;
+		featured[1]['startimage'] = startimage;
+		featured[1]['video'] = video;
+		featured[1]['image'] = image;
+	});
+
+	// swap featured people
+	$('#features img.person2').click(function () {
+		$f().unload();
+	
+		// store the old values
+		var name = featured[0]['name'];
+		var description = featured[0]['description'];
+		var startimage = featured[0]['startimage'];
+		var video = featured[0]['video'];
+		var image = featured[0]['image'];
+
+		// replace the values in the DOM
+		$('#crowdMemberName').html(featured[2]['name'].toLowerCase());
+		$('#crowdMemberDescription').html(featured[2]['description']);
+		$('#mainFeature img.person').attr('src',featured[2]['image']).attr('title',featured[2]['name']).attr('alt',featured[2]['name']);
+		$('#featured').attr('href',featured[2]['video']);
+		$('#featured').css('background-image','url('+featured[2]['startimage']+')');
+
+		// swap the featured person's image out, into the "small"
+		$('#features img.person2').attr('src',image).attr('title',name).attr('alt',name);
+
+		// swap the values in the array (2 -> 0, 0 -> 2)
+		featured[0]['name'] = featured[2]['name'];
+		featured[0]['description'] = featured[2]['description'];
+		featured[0]['startimage'] = featured[2]['startimage'];
+		featured[0]['video'] = featured[2]['video'];
+		featured[0]['image'] = featured[2]['image'];
+
+		featured[2]['name'] = name;
+		featured[2]['description'] = description;
+		featured[2]['startimage'] = startimage;
+		featured[2]['video'] = video;
+		featured[2]['image'] = image;
+	});
 });
