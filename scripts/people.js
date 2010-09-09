06dafe17f5f4a6489bb523ec8fed05e7510e8e92
Alan Orth <a.orth@cgiar.org>
2010-09-09 11:07:57 +0300

    Create a "swapPeople" function to lessen the amount of code in people.js.

--- scripts/people.js
+++ scripts/people.js
@@ -28,8 +28,7 @@ $(document).ready(function() {
 		});
 	});
 
-	// swap featured people
-	$('#features img.person1').click(function () {
+	function swapPeople(x) {
 		// store the old values
 		var name = featured[0]['name'];
 		var description = featured[0]['description'];
@@ -38,78 +37,47 @@ $(document).ready(function() {
 		var image = featured[0]['image'];
 
 		if(featuredPlayer.isLoaded()) {
-			featuredPlayer.getClip().update({url: featured[1]['video']});
+			featuredPlayer.getClip().update({url: featured[x]['video']});
 		}
 		else {
-			featuredPlayer.getClip(0).update({url: featured[1]['video']});
+			featuredPlayer.getClip(0).update({url: featured[x]['video']});
 		}
 
 		// replace the values in the DOM
-		$('#crowdMemberName').html(featured[1]['name'].toLowerCase());
-		$('#crowdMemberDescription').html(featured[1]['description']);
-		$('#mainFeature img.person').attr('src',featured[1]['image']).attr('title',featured[1]['name']).attr('alt',featured[1]['name']);
-		$('#featured').attr('href',featured[1]['video']);
-		$('#featured').css('background-image','url('+featured[1]['startimage']+')');
+		$('#crowdMemberName').html(featured[x]['name'].toLowerCase());
+		$('#crowdMemberDescription').html(featured[x]['description']);
+		$('#mainFeature img.person').attr('src',featured[x]['image']).attr('title',featured[x]['name']).attr('alt',featured[x]['name']);
+		$('#featured').attr('href',featured[x]['video']);
+		$('#featured').css('background-image','url('+featured[x]['startimage']+')');
 
 		// swap the featured person's image out, into the "small"
-		$('#features img.person1').attr('src',image).attr('title',name).attr('alt',name);
+		$('#features img.person'+x).attr('src',image).attr('title',name).attr('alt',name);
 
-		// swap the values in the array (1 -> 0, 0 -> 1)
-		featured[0]['name'] = featured[1]['name'];
-		featured[0]['description'] = featured[1]['description'];
-		featured[0]['startimage'] = featured[1]['startimage'];
-		featured[0]['video'] = featured[1]['video'];
-		featured[0]['image'] = featured[1]['image'];
+		// swap the values in the array (x -> 0, 0 -> x)
+		featured[0]['name'] = featured[x]['name'];
+		featured[0]['description'] = featured[x]['description'];
+		featured[0]['startimage'] = featured[x]['startimage'];
+		featured[0]['video'] = featured[x]['video'];
+		featured[0]['image'] = featured[x]['image'];
 
-		featured[1]['name'] = name;
-		featured[1]['description'] = description;
-		featured[1]['startimage'] = startimage;
-		featured[1]['video'] = video;
-		featured[1]['image'] = image;
+		featured[x]['name'] = name;
+		featured[x]['description'] = description;
+		featured[x]['startimage'] = startimage;
+		featured[x]['video'] = video;
+		featured[x]['image'] = image;
+	}
+
+	// swap featured people
+	$('#features img.person1').click(function () {
+		swapPeople(1);
 
 		// disable normal link behaviour
 		return false;
-
 	});
 
 	// swap featured people
 	$('#features img.person2').click(function () {
-		// store the old values
-		var name = featured[0]['name'];
-		var description = featured[0]['description'];
-		var startimage = featured[0]['startimage'];
-		var video = featured[0]['video'];
-		var image = featured[0]['image'];
-
-		if(featuredPlayer.isLoaded()) {
-			featuredPlayer.getClip().update({url: featured[2]['video']});
-		}
-		else {
-			featuredPlayer.getClip(0).update({url: featured[2]['video']});
-		}
-
-		// replace the values in the DOM
-		$('#crowdMemberName').html(featured[2]['name'].toLowerCase());
-		$('#crowdMemberDescription').html(featured[2]['description']);
-		$('#mainFeature img.person').attr('src',featured[2]['image']).attr('title',featured[2]['name']).attr('alt',featured[2]['name']);
-		$('#featured').attr('href',featured[2]['video']);
-		$('#featured').css('background-image','url('+featured[2]['startimage']+')');
-
-		// swap the featured person's image out, into the "small"
-		$('#features img.person2').attr('src',image).attr('title',name).attr('alt',name);
-
-		// swap the values in the array (2 -> 0, 0 -> 2)
-		featured[0]['name'] = featured[2]['name'];
-		featured[0]['description'] = featured[2]['description'];
-		featured[0]['startimage'] = featured[2]['startimage'];
-		featured[0]['video'] = featured[2]['video'];
-		featured[0]['image'] = featured[2]['image'];
-
-		featured[2]['name'] = name;
-		featured[2]['description'] = description;
-		featured[2]['startimage'] = startimage;
-		featured[2]['video'] = video;
-		featured[2]['image'] = image;
+		swapPeople(2);
 
 		// disable normal link behaviour
 		return false;
