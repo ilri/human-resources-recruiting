b58589cbce3297715c409aacf5e9cfa4a2fb07a3
Alan Orth <a.orth@cgiar.org>
2011-03-24 14:03:01 +0300

    Clean up header navigation links. Delete php files for To Do and People as we are no longer using those links in the header navigation

--- navigation.php
+++ navigation.php
@@ -7,7 +7,5 @@ $page = basename($_SERVER['PHP_SELF']);
 <div id="navcontainer">
 <ul id="navlist">
 <li><a href="http://www.ilri.org/" title="Home">Home</a></li>
-<li><a href="todo.php"<?php if($page == 'todo.php') echo 'id="current"'; ?>>To do</a></li>
-<li><a href="people.php" <?php if($page == 'people.php') echo 'id="current"'; ?>>People</a></li>
 </ul>
 </div>
