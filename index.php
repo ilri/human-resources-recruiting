eb14f2c510f1fc684694c4a52f6958f5bb6c747e
Alan Orth <alan.orth@gmail.com>
2011-03-29 22:42:28 +0300

    Update jQuery to 1.5.1 Update jQuery UI to 1.8.11

--- index.php
+++ index.php
@@ -423,8 +423,8 @@
 
 </div> <!-- //#outer -->
 
-<script type="text/javascript" src="scripts/jquery.min.js"></script>
-<script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-1.5.1.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.8.11.min.js"></script>
 <script type="text/javascript" src="scripts/jquery.flip.min.js"></script>
 <script type="text/javascript" src="scripts/flowplayer-3.2.6.min.js"></script>
 <script type="text/javascript" src="scripts/jquery.tools.min.js"></script>
